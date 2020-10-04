ae668720c8bf0e462d74fb2c8ce2e8e0142366ac
Srinibas Misra <srinibas.misra97@gmail.com>
2020-10-04 13:03:06 +0530

    added why are we using python program

---
+++ why_python.py
@@ -0,0 +1,9 @@
+question = input("Ask your question:")
+
+if question == "Why are we using python?":
+    print("It is as universal as English!")
+    enter = input()
+    print("It is the simplest language to learn coding in!")
+    enter = input()
+
+print("Happy Coding!")
